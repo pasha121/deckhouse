686ade92b8d9263d293614de907e62e126e6cf39
Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>
2022-11-04 09:36:21 +0300

    ++
    Signed-off-by: Nikolay Mitrofanov <nikolay.mitrofanov@flant.com>

--- deckhouse-controller/cmd/deckhouse-controller/register-go-hooks.go
+++ deckhouse-controller/cmd/deckhouse-controller/register-go-hooks.go
@@ -21,6 +21,7 @@ import (
 	_ "github.com/deckhouse/deckhouse/global-hooks"
 	_ "github.com/deckhouse/deckhouse/global-hooks/discovery"
 	_ "github.com/deckhouse/deckhouse/global-hooks/resources"
+	_ "github.com/deckhouse/deckhouse/global-hooks/telemetry"
 	_ "github.com/deckhouse/deckhouse/modules/000-common/hooks"
 	_ "github.com/deckhouse/deckhouse/modules/002-deckhouse/hooks"
 	_ "github.com/deckhouse/deckhouse/modules/010-operator-prometheus-crd/hooks"
